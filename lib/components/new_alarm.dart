03372c723b5ac143c47c1ff1d07464f06a91d491
turap <turapro3443@mail.ru>
2024-06-05 19:36:22 +0300

    last update

--- lib/components/new_alarm.dart
+++ lib/components/new_alarm.dart
@@ -1,58 +1,140 @@
+import 'package:progr_alarm/wakes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:progr_alarm/models/alarm.dart';
+import 'package:provider/provider.dart';
 
-class AddAlarm extends StatelessWidget {
-  const AddAlarm({super.key});
+class AddAlarm extends StatefulWidget {
+  AddAlarm({super.key});
+
+  @override
+  State<AddAlarm> createState() => _AddAlarmState();
+}
+
+class _AddAlarmState extends State<AddAlarm> {
+  final int listTileLen = 4;
+
+  Duration time =
+      Duration(hours: DateTime.now().hour, minutes: DateTime.now().minute);
+
+  String defaultName = "Будильник";
+
+  String defaultMelody = "Сигнал Тревоги";
+
+  bool defaultRepeatMelody = true;
+
+  bool isRepeatAlarm = false;
+
+  final List<String> items = [
+    "Повтор",
+    "Название",
+    "Мелодия",
+    "Повторение сигнала"
+  ];
+
+  final List<String> selectedItems = [];
+  final List<String> days = ["пн", "вт"];
 
   @override
   Widget build(BuildContext context) {
+    final alarmList = Provider.of<AlarmList>(context);
+
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 19, 19, 19),
+      backgroundColor: Color.fromARGB(255, 29, 29, 29),
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Добавить будильник",
-            style: TextStyle(color: Colors.white, fontSize: 16)),
-        backgroundColor: Color.fromARGB(255, 14, 13, 13),
+        title: Text(
+          "Добавить будильник",
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+        backgroundColor: Color.fromARGB(255, 34, 34, 35),
         leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.orange),
-            onPressed: () => Navigator.pop(context)),
+          icon: Icon(Icons.arrow_back, color: Colors.orange),
+          onPressed: () => Navigator.pop(context),
+        ),
         actions: [
           TextButton(
-              onPressed: () {},
-              child: Text("Добавить", style: TextStyle(color: Colors.orange)))
+            onPressed: () {
+              setState(() {
+                alarmList.wakes
+                    .add(Alarm(name: defaultName, isActive: true, time: time));
+                alarmList.notifyListeners();
+              });
+              Navigator.pop(context);
+            },
+            child: Text(
+              "Добавить",
+              style: TextStyle(color: Colors.orange),
+            ),
+          )
         ],
       ),
       body: Container(
-        height: MediaQuery.of(context).size.height * 0.8,
-        width: double.infinity,
         child: Column(
           children: [
-            Expanded(
-              child: Container(
-                height: 200,
-                child: CupertinoTheme(
-                  data: CupertinoThemeData(
-                    textTheme: CupertinoTextThemeData(
-                      dateTimePickerTextStyle: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
+            SizedBox(
+              height: 200,
+              child: CupertinoTheme(
+                data: CupertinoThemeData(
+                  textTheme: CupertinoTextThemeData(
+                    dateTimePickerTextStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
                     ),
                   ),
-                  child: CupertinoDatePicker(
-                    onDateTimeChanged: (value) {
-                      print(value);
-                    },
-                    use24hFormat: true,
-                    mode: CupertinoDatePickerMode.time,
-                  ),
+                ),
+                child: CupertinoDatePicker(
+                  onDateTimeChanged: (value) {
+                    time =
+                        new Duration(hours: value.hour, minutes: value.minute);
+                  },
+                  use24hFormat: true,
+                  mode: CupertinoDatePickerMode.time,
                 ),
               ),
             ),
-            Container(
-              height: 100,
-              color: Colors.grey,
-              child: Text("hjwedawd"),
+            Padding(padding: EdgeInsets.only(top: 30)),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                child: CupertinoListSection.insetGrouped(
+                  header: const Text('Настройки',
+                      style: TextStyle(color: Colors.white)),
+                  backgroundColor: Color.fromARGB(255, 29, 29, 29),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: const Color(0xFF2C2C2E),
+                  ),
+                  children: <CupertinoListTile>[
+                    CupertinoListTile(
+                        title: const Text('Повтор',
+                            style: TextStyle(color: Colors.white)),
+                        additionalInfo: Text('Никогда',
+                            style: TextStyle(color: Colors.white)),
+                        onTap: () {}),
+                    CupertinoListTile(
+                      title: const Text('Название',
+                          style: TextStyle(color: Colors.white)),
+                      additionalInfo: Text(defaultName,
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    CupertinoListTile(
+                        title: const Text('Мелодия',
+                            style: TextStyle(color: Colors.white)),
+                        additionalInfo: Text(defaultMelody,
+                            style: TextStyle(color: Colors.white)),
+                        onTap: () {}),
+                    CupertinoListTile(
+                        title: const Text('Повторение сигнала',
+                            style: TextStyle(color: Colors.white)),
+                        additionalInfo: CupertinoSwitch(
+                          value: isRepeatAlarm,
+                          onChanged: (value) {},
+                        ),
+                        onTap: () {}),
+                  ],
+                ),
+              ),
             )
           ],
         ),
--- lib/wakes.dart
+++ lib/wakes.dart
@@ -1,16 +1,26 @@
-import 'package:progr_alarm/classes/alarm.dart';
+import 'package:flutter/foundation.dart';
+import 'package:progr_alarm/models/alarm.dart';
 
-List<Alarm> wakes = [
-  Alarm(
-      name: "Учеба",
-      isActive: true,
-      time: const Duration(hours: 9, minutes: 30)),
-  Alarm(
-      name: "Работа",
-      isActive: false,
-      time: const Duration(hours: 13, minutes: 50)),
-  Alarm(
-      name: "Тренировка",
-      isActive: true,
-      time: const Duration(hours: 21, minutes: 10))
-];
+class AlarmList extends ChangeNotifier {
+  List<Alarm> _wakes = [
+    Alarm(
+        name: "Учеба",
+        isActive: true,
+        time: const Duration(hours: 9, minutes: 30)),
+    Alarm(
+        name: "Работа",
+        isActive: false,
+        time: const Duration(hours: 13, minutes: 50)),
+    Alarm(
+        name: "Тренировка",
+        isActive: true,
+        time: const Duration(hours: 21, minutes: 10))
+  ];
+
+  List<Alarm> get wakes => _wakes;
+
+  set wakes(List<Alarm> value) {
+    _wakes = value;
+    notifyListeners();
+  }
+}
